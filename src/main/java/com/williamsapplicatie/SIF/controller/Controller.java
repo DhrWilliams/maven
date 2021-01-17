2454cf0878903c02f650e9622ab559c62af07a3a
Williamsapplicatie <dhr.cwilliams@gmail.com>
2021-01-17 14:43:28 +0100

    testing automatisatie

---
+++ src/main/java/com/williamsapplicatie/SIF/controller/Controller.java
@@ -0,0 +1,18 @@
+package com.williamsapplicatie.SIF.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+	
+	
+	
+	
+	@RequestMapping("/tomcat")
+	public String tro() {
+		
+		return "Het werkt goed";
+	}
+
+}
