80244f4087ac11f48b8c34b5f64181ee0a833122
Williamsapplicatie <dhr.cwilliams@gmail.com>
2021-01-17 02:47:10 +0100

    Initial commit

---
+++ src/test/java/com/williamsapplicatie/SIF/SifApplicationTests.java
@@ -0,0 +1,13 @@
+package com.williamsapplicatie.SIF;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SifApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
